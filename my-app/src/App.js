4bd4dac3105766343825af1dd8984d4d564008eb
unAlike <Ryan.Ungerleider@gmail.com>
2023-04-05 23:12:54 -0500

    Nodejs

---
+++ my-app/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+	return (
+		<div>
+			<nav class="sidenav">
+        <table>
+          <td>
+            Home
+          </td>
+          <td>
+            Projects
+          </td>
+          <td>
+            Contact
+          </td>
+        </table>
+			</nav>
+      <main className='main'>
+        <div className='main'>
+          <p>
+            Hello
+          </p>
+        </div>
+      </main>
+    </div>
+	)
+}
+
+export default App
