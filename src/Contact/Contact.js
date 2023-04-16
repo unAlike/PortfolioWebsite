6df5d27a5986f85d62450c6fe98caae16138a72d
unAlike <Ryan.Ungerleider@gmail.com>
2023-04-15 23:49:32 -0500

    Updated Colors

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -6,7 +6,7 @@ function Contact() {
   return (
     <div className="Contact">
       <div className='about-me'>
-        <h1 className='Contact-header'>Contact me!</h1>
+        <h1 className='Contact-header'>Contact Information</h1>
       </div>
       
     </div>
