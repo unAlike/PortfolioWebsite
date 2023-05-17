01f9d3c6378265f94ee7a00757c76106993d9583
unAlike <Ryan.Ungerleider@gmail.com>
2023-05-17 13:41:57 -0500

    Filler for review

--- src/About/About.js
+++ src/About/About.js
@@ -23,11 +23,10 @@ function About() {
             </div>
             <div class='content-flex'>
               <span id='bio'>
-                I am very passionate about technology and how we can utilize/benefit from it. 
-                I take the saying "Work smarter not harder" very close to heart because I truly believe that there is always a better way.
-                I constantly strive to improve and learn whenever given the opportunity.
-                Exploring all types of technology and software has granted me experience in almost every field.
-                I am confident in my ability to solve and learn from any problem thrown my way.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+               Rhoncus est pellentesque elit ullamcorper dignissim cras tincidunt. Accumsan in nisl nisi scelerisque eu ultrices.
+                Tellus in metus vulputate eu scelerisque felis. Gravida dictum fusce ut placerat orci. 
+                In nibh mauris cursus mattis molestie a iaculis at erat. Dui vivamus arcu felis bibendum ut tristique et.
               </span>
               <img src={pfp} id='pfp'/>
             </div>
@@ -38,11 +37,12 @@ function About() {
           <div class='content-flex'>
             <img src={pfp} id='pfp'/>
               <span id='bio'>
-                I am very passionate about technology and how we can utilize/benefit from it. 
-                I take the saying "Work smarter not harder" very close to heart because I truly believe that there is always a better way.
-                I constantly strive to improve and learn whenever given the opportunity.
-                Exploring all types of technology and software has granted me experience in almost every field.
-                I am confident in my ability to solve and learn from any problem thrown my way.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+               Rhoncus est pellentesque elit ullamcorper dignissim cras tincidunt. Accumsan in nisl nisi scelerisque eu ultrices.
+                Tellus in metus vulputate eu scelerisque felis. Gravida dictum fusce ut placerat orci. 
+                In nibh mauris cursus mattis molestie a iaculis at erat. Dui vivamus arcu felis bibendum ut tristique et. 
+                Id volutpat lacus laoreet non curabitur gravida arcu ac tortor. A iaculis at erat pellentesque. 
+                
               </span>
             </div>
           </div>
